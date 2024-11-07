d5991110a393d54007bb80de73210d996d7ba598
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2024-11-07 00:33:29 -0300

    implementando a logica dos botoes numericos

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -5,8 +5,40 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrls: ['./calculator.component.css'],
 })
 export class CalculatorComponent {
+  currentValue: string = '0';
+  previousValue: string = '';
+  operation: string | null = null;
 
+  inputNumber(num: string): void {
+    if (this.currentValue === '0') {
+      this.currentValue = num;
+    } else {
+      this.currentValue += num;
+    }
+  }
+
+  calcResult() {
+    let result: number;
+    const prev = parseFloat(this.previousValue);
+    const current = parseFloat(this.currentValue);
+
+    switch (this.operation) {
+      case '+':
+        result = prev + current;
+        break;
+    }
+  }
+
+  clearEntry() {
+    this.currentValue = '0';
+  }
+
+  clearAll() {
+    this.currentValue = '0';
+    this.previousValue = '';
+    this.operation = null;
+  }
 }
