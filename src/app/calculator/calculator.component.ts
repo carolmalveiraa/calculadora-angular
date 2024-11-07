0df81bc63b84c87d3c82a65c6b943e9b64c86191
Carol Malveira <carolmalveiraa12@icloud.com>
2024-11-07 00:43:30 -0300

    refatora componente da calculadora: atualiza visor e lógica de entrada

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -5,83 +5,40 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrls: ['./calculator.component.css'],
 })
 export class CalculatorComponent {
-  display: string = '';
-  valor1: number | null = null;
-  valor2: number | null = null;
-  operador: string | null = null;
+  currentValue: string = '0';
+  previousValue: string = '';
+  operation: string | null = null;
 
-  adicionarNumero(numero: string) {
-    console.log("Número adicionado:", numero);
-    this.display += numero;
-  }
-  
-
-  escolherOperador(operador: string) {
-      if (this.display !== '') {
-      this.valor1 = parseFloat(this.display);
-      this.operador = operador;
-      this.display = '';
+  inputNumber(num: string): void {
+    if (this.currentValue === '0') {
+      this.currentValue = num;
+    } else {
+      this.currentValue += num;
     }
   }
 
-  calcular() {
-    if (this.valor1 !== null && this.operador !== null) {
-      this.valor2 = parseFloat(this.display);
-      let resultado = 0;
+  calcResult() {
+    let result: number;
+    const prev = parseFloat(this.previousValue);
+    const current = parseFloat(this.currentValue);
 
-      switch (this.operador) {
-        case '+':
-          resultado = this.valor1 + this.valor2;
-          break;
-        case '-':
-          resultado = this.valor1 - this.valor2;
-          break;
-        case '*':
-          resultado = this.valor1 * this.valor2;
-          break;
-        case '/':
-          if (this.valor2!== 0) {
-            resultado = this.valor1 / this.valor2;
-          } else {
-            alert('Não é possível dividir por zero.');
-            return;
-          }
-          break;
-        default:
-          alert('Operador inválido.');
-          return;
-      }
-
-      this.display = resultado.toString();
-      this.valor1 = null;
-      this.valor2 = null;
-      this.operador = null;
+    switch (this.operation) {
+      case '+':
+        result = prev + current;
+        break;
     }
   }
 
-  limpar() {
-    this.display = '';
-    this.valor1 = null;
-    this.valor2 = null;
-    this.operador = null;
+  clearEntry() {
+    this.currentValue = '0';
   }
 
-  limparEntrada() {
-    this.display = '';
-  }
-
-  inverterSinal() {
-    if (this.display !== '') {
-      this.display = (parseFloat(this.display) * -1).toString();
-    }
-  }
-
-  calcularPercentual() {
-    if (this.display !== '') {
-      this.display = (parseFloat(this.display) / 100).toString();
-    }
+  clearAll() {
+    this.currentValue = '0';
+    this.previousValue = '';
+    this.operation = null;
   }
 }
