3a195ad2e378f604f6554b3b3b7680fa013e2941
Carol Malveira <carolmalveiraa12@icloud.com>
2024-11-07 00:19:28 -0300

    adiciona visor e lógica de cálculo à calculadora

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -8,5 +8,80 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
+  display: string = '';
+  valor1: number | null = null;
+  valor2: number | null = null;
+  operador: string | null = null;
 
+  adicionarNumero(numero: string) {
+    console.log("Número adicionado:", numero);
+    this.display += numero;
+  }
+  
+
+  escolherOperador(operador: string) {
+      if (this.display !== '') {
+      this.valor1 = parseFloat(this.display);
+      this.operador = operador;
+      this.display = '';
+    }
+  }
+
+  calcular() {
+    if (this.valor1 !== null && this.operador !== null) {
+      this.valor2 = parseFloat(this.display);
+      let resultado = 0;
+
+      switch (this.operador) {
+        case '+':
+          resultado = this.valor1 + this.valor2;
+          break;
+        case '-':
+          resultado = this.valor1 - this.valor2;
+          break;
+        case '*':
+          resultado = this.valor1 * this.valor2;
+          break;
+        case '/':
+          if (this.valor2!== 0) {
+            resultado = this.valor1 / this.valor2;
+          } else {
+            alert('Não é possível dividir por zero.');
+            return;
+          }
+          break;
+        default:
+          alert('Operador inválido.');
+          return;
+      }
+
+      this.display = resultado.toString();
+      this.valor1 = null;
+      this.valor2 = null;
+      this.operador = null;
+    }
+  }
+
+  limpar() {
+    this.display = '';
+    this.valor1 = null;
+    this.valor2 = null;
+    this.operador = null;
+  }
+
+  limparEntrada() {
+    this.display = '';
+  }
+
+  inverterSinal() {
+    if (this.display !== '') {
+      this.display = (parseFloat(this.display) * -1).toString();
+    }
+  }
+
+  calcularPercentual() {
+    if (this.display !== '') {
+      this.display = (parseFloat(this.display) / 100).toString();
+    }
+  }
 }
